20d363c58b246c6592bd914739df3d078a34eb0d
Narongvit Promkhana <narongvit.thon@gmail.com>
2020-08-08 14:04:35 +0700

    add media root and add field image_profile in worker

--- safety/safety/settings/local.py
+++ safety/safety/settings/local.py
@@ -1,1 +1,4 @@
 from .base import *
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR) / 'media'
--- safety/workers/tests/test_models.py
+++ safety/workers/tests/test_models.py
@@ -1,4 +1,9 @@
+import os
+from unittest.mock import MagicMock
+
 from django.test import TestCase
+from django.core.files import File
+
 from ..models import Worker
 
 
@@ -12,10 +17,15 @@ class TestWorker(TestCase):
         secondary_phone = '082-524-818x'
         address = 'Geeky Base All Star'
 
+        # mock image
+        image_profile = MagicMock(spec=File)
+        image_profile.name = 'bothon.png'
+
         # When
         worker = Worker.objects.create(
             first_name=first_name,
             last_name=last_name,
+            image_profile=image_profile,
             is_available=is_available,
             primary_phone=primary_phone,
             secondary_phone=secondary_phone,
@@ -25,7 +35,10 @@ class TestWorker(TestCase):
         # Then
         self.assertEqual(worker.first_name, first_name)
         self.assertEqual(worker.last_name, last_name)
+        self.assertEqual(worker.image_profile, image_profile)
         self.assertTrue(worker.is_available)
         self.assertEqual(worker.primary_phone, primary_phone)
         self.assertEqual(worker.secondary_phone, secondary_phone)
         self.assertEqual(worker.address, address)
+
+        os.remove('media/bothon.png')
