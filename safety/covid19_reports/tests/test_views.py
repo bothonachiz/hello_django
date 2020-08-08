14a274492fa2435fda4f5278b1a10c50282cb004
Narongvit Promkhana <narongvit.thon@gmail.com>
2020-08-08 11:41:12 +0700

    add mock request http

--- safety/covid19_reports/tests/test_views.py
+++ safety/covid19_reports/tests/test_views.py
@@ -1,6 +1,38 @@
 from django.test import TestCase
 
+from unittest.mock import patch
+
+
 class TestCovid19ReportView(TestCase):
-    def test_view_should_be_accessible(self):
+
+    @patch('covid19_reports.views.requests.get')
+    def test_view_should_be_accessible(self, _):
         response = self.client.get('/covid19-reports/')
         self.assertEqual(response.status_code, 200)
+
+    @patch('covid19_reports.views.requests.get')
+    def test_view_should_call_covid19_api(self, mock):
+        self.client.get('/covid19-reports/')
+        mock.assert_called_once_with(
+            'https://covid19.th-stat.com/api/open/today',
+        )
+
+    @patch('covid19_reports.views.requests.get')
+    def test_view_should_render_number_of_confirmed(self, mock):
+        mock_response = mock.return_value
+        mock_response.json.return_value = { #return_value replace ()
+            "Confirmed": 3345,
+            "Recovered": 3148,
+            "Hospitalized": 139,
+            "Deaths": 58,
+            "NewConfirmed": 50,
+            "NewRecovered": 0,
+            "NewHospitalized": 15,
+            "NewDeaths": 0,
+            "UpdateDate": "07/08/2020 11:36",
+            "Source": "https://covid19.th-stat.com/",
+            "DevBy": "https://www.kidkarnmai.com/",
+            "SeverBy": "https://smilehost.asia/"
+        }
+        response = self.client.get('/covid19-reports/')
+        self.assertContains(response, 'NewConfirmed: 50')
--- safety/covid19_reports/views.py
+++ safety/covid19_reports/views.py
@@ -1,7 +1,14 @@
 from django.views import View
 from django.http import HttpResponse
 
+import requests
+
 class Covid19ReportView(View):
     def get(self, request):
-        return HttpResponse()
+        response = requests.get('https://covid19.th-stat.com/api/open/today')
+        data = response.json()
+        new_confirm = data['NewConfirmed']
+        
+        return HttpResponse(f'NewConfirmed: {new_confirm}')
+        
     
