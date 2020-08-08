eb9052c02d37486de989b07c7f9cd77a550383db
Narongvit Promkhana <narongvit.thon@gmail.com>
2020-08-08 16:52:03 +0700

    add fk worker

--- safety/certificates/models.py
+++ safety/certificates/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+from workers.models import Worker
+
 
 class Certificate(models.Model):
     name = models.CharField(max_length=50)
     issue_by = models.CharField(max_length=50)
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
--- safety/certificates/tests/test_models.py
+++ safety/certificates/tests/test_models.py
@@ -2,10 +2,21 @@ from django.test import TestCase
 from django.core.files import File
 
 from ..models import Certificate
+from workers.models import Worker
 
 
 class TestCertificate(TestCase):
     def test_certificater_shoud_have_defined_fields(self):
+
+        worker = Worker.objects.create(
+            first_name='Narongvit',
+            last_name='Promkhana',
+            is_available=True,
+            primary_phone='087-784-878x',
+            secondary_phone='082-524-818x',
+            address='Geeky Base All Star',
+        )
+
         # Given
         name = 'Django Certificate by ODDS'
         issue_by = 'Proof'
@@ -13,9 +24,11 @@ class TestCertificate(TestCase):
         # When
         certificate = Certificate.objects.create(
             name=name,
-            issue_by=issue_by
+            issue_by=issue_by,
+            worker=worker
         )
 
         # Then
         assert certificate.name == name
         assert certificate.issue_by == issue_by
+        assert certificate.worker.first_name == worker.first_name
